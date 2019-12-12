ed47e8d7a423e7c257a7fa52adfc809ee5d89d6c
actor20170211030627 <1455198886@qq.com>
2019-12-12 17:05:15 +0800

    .
    update GridTableRadioGroup

--- app/src/main/java/com/actor/testapplication/activity/TestActivity.java
+++ app/src/main/java/com/actor/testapplication/activity/TestActivity.java
@@ -2,9 +2,11 @@ package com.actor.testapplication.activity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.actor.testapplication.R;
+import com.actor.testapplication.widget.GridTableRadioGroup;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,9 +15,11 @@ import butterknife.OnClick;
 public class TestActivity extends BaseActivity {
 
     @BindView(R.id.tv_result)//显示结果
-    TextView     tvResult;
+    TextView            tvResult;
     @BindView(R.id.btn)
-    Button       btn;
+    Button              btn;
+    @BindView(R.id.grid_table_radio_group)
+    GridTableRadioGroup gridTableRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +28,17 @@ public class TestActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         setTitle("测试页面");
+        gridTableRadioGroup.setOnCheckedChangeListener(new GridTableRadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId, int position, boolean reChecked) {
+                tvResult.setText(getStringFormat("checkedId = %d, pos = %d, reChecked = %b", checkedId, position, reChecked));
+            }
+        });
     }
 
     @OnClick(R.id.btn)
     public void onViewClicked() {
+        int pos = gridTableRadioGroup.getCheckedPosition();
+        gridTableRadioGroup.setCheckedPosition(1);
     }
 }
--- app/src/main/java/com/actor/testapplication/widget/GridTableRadioGroup.java
+++ app/src/main/java/com/actor/testapplication/widget/GridTableRadioGroup.java
@@ -1,11 +1,18 @@
 package com.actor.testapplication.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.IdRes;
 import android.support.annotation.IntRange;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.widget.AppCompatRadioButton;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.actor.testapplication.R;
@@ -16,39 +23,71 @@ import com.actor.testapplication.R;
  * Company    : 重庆市了赢科技有限公司 http://www.liaoin.com/
  * Author     : 李大发
  * Date       : 2019/7/10 on 17:21
+ *
+ * 1.左侧红点显示类型, 默认visible
+ * @see R.styleable#GridTableEditText_gteRedStarVisiable    //visible/invisible/gone
+ * 2.左侧提示文字
+ * @see R.styleable#GridTableRadioGroup_gtrItemName         //请选择性别：
+ * 3.右侧RadioButton的gravity
+ * @see R.styleable#GridTableRadioGroup_gtrGravity          //start|centerVertical
+ * 4.RadioButton1的值
+ * @see R.styleable#GridTableRadioGroup_gtrRb1Text          //男
+ * 5.RadioButton2的值
+ * @see R.styleable#GridTableRadioGroup_gtrRb2Text          //女
+ * 6.RadioButton3的值
+ * @see R.styleable#GridTableRadioGroup_gtrRb3Text          //未知
+ * 7.选中第几个, 默认0
+ * @see R.styleable#GridTableRadioGroup_gtrCheckedPosition  //0
+ *
  * @version 1.0
+ * @version 1.1 增加gtrGravity属性
+ * @version 1.1.1 修改方法: {@link #setCheckedPosition(int)} 增加重复选中监听
+ *
+ * TODO: 2019/7/10 动态增加RadioButton
  */
 public class GridTableRadioGroup extends LinearLayout {
 
-    private static final String NAME_SPACE = "http://schemas.android.com/apk/res-auto";
     private TextView tvRedStar;
     private TextView tv1;
-//    private RadioGroup rg;
+    private RadioGroup radioGroup;
     private AppCompatRadioButton rb1;
     private AppCompatRadioButton rb2;
     private AppCompatRadioButton rb3;
+    private OnCheckedChangeListener onCheckedChangeListener;
 
     public GridTableRadioGroup(Context context) {
-        this(context,null);
+        super(context);
+        init(context, null, -1);
     }
 
-    public GridTableRadioGroup(Context context, AttributeSet attrs) {
-        this(context, attrs,-1);
+    public GridTableRadioGroup(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, -1);
     }
 
-    public GridTableRadioGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+    public GridTableRadioGroup(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public GridTableRadioGroup(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs, defStyleAttr);
+    }
+
+    protected void init(Context context, AttributeSet attrs, int defStyleAttr) {
         /**
          * 给当前空的布局填充内容
          * 参3:A view group that will be the parent.
          * 传null表示当前布局没有父控件,大部分都传null
          * 传this表示已当前相对布局为这个布局的父控件,这样做了以后,当前空的布局就有内容了
          */
-        //1.给SettingItemView(RelatuveLayout)填充布局
+        //填充布局
         View inflate = View.inflate(context, R.layout.item_grid_table_radio_group, this);
         tvRedStar = inflate.findViewById(R.id.tv_red_star_for_gtr);
         tv1 = inflate.findViewById(R.id.tv_item_for_gtr);
-//        rg = (RadioGroup) inflate.findViewById(R.id.rg);
+        radioGroup = inflate.findViewById(R.id.rg_for_gtr);
         rb1 = inflate.findViewById(R.id.rb_1_for_gtr);
         rb2 = inflate.findViewById(R.id.rb_2_for_gtr);
         rb3 = inflate.findViewById(R.id.rb_3_for_gtr);
@@ -56,14 +95,22 @@ public class GridTableRadioGroup extends LinearLayout {
         if (attrs == null) return;
 
         //读取自定义属性值
-        int visiable = attrs.getAttributeIntValue(NAME_SPACE, "gtrRedStarVisiable", 0);
-        tvRedStar.setVisibility(visiable * 4);//设置红点是否显示
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.GridTableRadioGroup);
+        //红点是否显示
+        int visiable = typedArray.getInt(R.styleable.GridTableRadioGroup_gtrRedStarVisiable, 0);
+        //左侧TextView的Text
+        String gtrItemName = typedArray.getString(R.styleable.GridTableRadioGroup_gtrItemName);
+        //radioButton1的值
+        String gtrRb1Text = typedArray.getString(R.styleable.GridTableRadioGroup_gtrRb1Text);
+        String gtrRb2Text = typedArray.getString(R.styleable.GridTableRadioGroup_gtrRb2Text);
+        String gtrRb3Text = typedArray.getString(R.styleable.GridTableRadioGroup_gtrRb3Text);
+        //默认选中第几个
+        int gtrCheckedPosition = typedArray.getInt(R.styleable.GridTableRadioGroup_gtrCheckedPosition, 0);
+        //RadioButton的居中gravity
+        int gravity = typedArray.getInt(R.styleable.GridTableRadioGroup_gtrGravity, Gravity.START | Gravity.CENTER_VERTICAL);
+        typedArray.recycle();
 
-        String gtrItemName = attrs.getAttributeValue(NAME_SPACE, "gtrItemName");//TextView的值
-        String gtrRb1Text = attrs.getAttributeValue(NAME_SPACE, "gtrRb1Text");
-        String gtrRb2Text = attrs.getAttributeValue(NAME_SPACE, "gtrRb2Text");
-        String gtrRb3Text = attrs.getAttributeValue(NAME_SPACE, "gtrRb3Text");
-        int gtrCheckedPosition = attrs.getAttributeIntValue(NAME_SPACE, "gtrCheckedPosition", 0);
+        tvRedStar.setVisibility(visiable * 4);//设置红点是否显示
         if (gtrItemName != null) tv1.setText(gtrItemName);
         if (gtrRb1Text != null) rb1.setText(gtrRb1Text);
         if (gtrRb2Text != null) rb2.setText(gtrRb2Text);
@@ -73,27 +120,38 @@ public class GridTableRadioGroup extends LinearLayout {
             rb3.setText(gtrRb3Text);
         }
         setCheckedPosition(gtrCheckedPosition);
+        radioGroup.setGravity(gravity);
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (onCheckedChangeListener != null) onCheckedChangeListener.onCheckedChanged(group, checkedId, getCheckedPosition(), false);
+        });
     }
 
     /**
-     * 获取红点
-     * @return
+     * @return 获取红点
      */
     public TextView getTextViewRedStar() {
         return tvRedStar;
     }
 
     /**
-     * 返回Item的TextView
-     * @return
+     * @return 返回Item的TextView
      */
     public TextView getTextViewItem() {
         return tv1;
     }
 
+    /**
+     * @return 返回RadioGroup
+     */
+    public RadioGroup getRadioGroup() {
+        return radioGroup;
+    }
+
     public void setCheckedPosition(@IntRange(from = 0, to = 2) int position) {
+        int checkedPosition = getCheckedPosition();
         switch (position) {
             case 1:
+//                radioGroup.check(R.id.rb_2_for_gtr);//这种方式不行, 会回调多次
                 rb2.setChecked(true);
                 break;
             case 2:
@@ -103,12 +161,38 @@ public class GridTableRadioGroup extends LinearLayout {
                 rb1.setChecked(true);
                 break;
         }
+        //重复选中
+        if (onCheckedChangeListener != null && checkedPosition == position) {
+            onCheckedChangeListener.onCheckedChanged(radioGroup, radioGroup.getCheckedRadioButtonId(), position, true);
+        }
     }
 
     public int getCheckedPosition() {
         if (rb1.isChecked()) return 0;
         if (rb2.isChecked()) return 1;
         if (rb3.isChecked()) return 2;
-        return 0;
+        return -1;//radioGroup.clearCheck();
+    }
+
+    /**
+     * @param listener 设置选中监听
+     */
+    public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
+        this.onCheckedChangeListener = listener;
+
+    }
+
+    public OnCheckedChangeListener getOnCheckedChangeListener() {
+        return onCheckedChangeListener;
+    }
+
+    public interface OnCheckedChangeListener {
+        /**
+         * @param group radioGroup
+         * @param checkedId 选中的Checkbox的id
+         * @param position 第几个position
+         * @param reChecked 是否是重复选中
+         */
+        void onCheckedChanged(RadioGroup group, @IdRes int checkedId, int position, boolean reChecked);
     }
 }
