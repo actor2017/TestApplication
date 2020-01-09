e8735d3f39bdf11043d4160e3fd037ecb08fa2d0
actor20170211030627 <1455198886@qq.com>
2020-01-09 22:54:31 +0800

    .

--- app/src/main/java/com/actor/testapplication/activity/TestActivity.java
+++ app/src/main/java/com/actor/testapplication/activity/TestActivity.java
@@ -1,11 +1,21 @@
 package com.actor.testapplication.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SectionIndexer;
 import android.widget.TextView;
 
 import com.actor.testapplication.R;
+import com.actor.testapplication.widget.EaseSidebar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,11 +29,19 @@ import butterknife.OnClick;
 public class TestActivity extends BaseActivity {
 
     @BindView(R.id.tv_result)//显示结果
-    TextView tvResult;
+            TextView    tvResult;
     @BindView(R.id.edit_text)
-    EditText editText;
+            EditText    editText;
     @BindView(R.id.btn)
-    Button   btn;
+            Button      btn;
+
+    @BindView(R.id.list)
+            ListView    list;
+    @BindView(R.id.sidebar)
+            EaseSidebar sidebar;
+    @BindView(R.id.floating_header)
+            TextView    floatingHeader;
+    private List<String> items = new ArrayList<>();
 
 
     @Override
@@ -33,6 +51,67 @@ public class TestActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         setTitle("Test测试页面");
+        sidebar.setListView(list);
+        for (int i = 0; i < 30; i++) {
+            items.add("item " + i);
+        }
+        list.setAdapter(new MyAdapter());
+    }
+
+    private class MyAdapter extends BaseAdapter implements SectionIndexer {
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if(convertView == null){
+                convertView = getLayoutInflater().inflate(R.layout.ease_row_contact, parent, false);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            }else{
+                holder = (ViewHolder) convertView.getTag();
+            }
+            holder.nameView.setText(items.get(position));
+            return convertView;
+        }
+
+        @Override
+        public Object[] getSections() {
+            return new Object[0];
+        }
+
+        @Override
+        public int getPositionForSection(int sectionIndex) {
+            return 0;
+        }
+
+        @Override
+        public int getSectionForPosition(int position) {
+            return 0;
+        }
+    }
+
+    private static class ViewHolder {
+        ImageView avatar;
+        TextView nameView;
+        public ViewHolder(View view) {
+            this.avatar = view.findViewById(R.id.iv);
+            this.nameView = view.findViewById(R.id.tv);
+        }
     }
 
     @OnClick(R.id.btn)
