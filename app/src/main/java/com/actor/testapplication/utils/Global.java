75288f6cbd614a559c7f6abf31c0d435f1e0f9ca
actor20170211030627 <1455198886@qq.com>
2020-01-11 17:06:52 +0800

    .

--- app/src/main/java/com/actor/testapplication/utils/Global.java
+++ app/src/main/java/com/actor/testapplication/utils/Global.java
@@ -10,18 +10,34 @@ package com.actor.testapplication.utils;
  */
 public class Global {
 
-    //必须GET
-    public static final String CHECK_UPDATE = "https://raw.githubusercontent.com/" +
-            "actor20170211030627/" +
+    //github.com, GET请求
+//    public static final String CHECK_UPDATE = "https://raw.githubusercontent.com/" +
+//            "actor20170211030627/" +
+//            "TestApplication" +//项目名
+//            "/master/" +
+//            "app" +//模块名
+//            "/build/outputs/apk/debug/output.json";
+
+    //gitee.com码云, GET请求
+    public static final String CHECK_UPDATE = "https://gitee.com/actor2017/" +
             "TestApplication" +//项目名
-            "/master/" +
+            "/raw/master/" +
             "app" +//模块名
             "/build/outputs/apk/debug/output.json";
 
-    public static final String DOWNLOAD_URL = "https://raw.githubusercontent.com/" +
-            "actor20170211030627/" +
-            "TestApplication" +//项目名
-            "/master/" +
-            "app" +//模块名
+
+    //github.com
+//    public static final String DOWNLOAD_URL = "https://raw.githubusercontent.com/" +
+//            "actor20170211030627/" +
+//            "TestApplication" +//项目名
+//            "/master/" +
+//            "app" +//模块名
+//            "/build/outputs/apk/debug/app-debug.apk";
+
+    //gitee.com码云
+    public static final String DOWNLOAD_URL = "https://gitee.com/actor2017/" +
+            "TestApplication" +
+            "/raw/master/" +
+            "app" +
             "/build/outputs/apk/debug/app-debug.apk";
 }
