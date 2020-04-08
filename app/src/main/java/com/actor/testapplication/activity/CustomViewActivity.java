dfca4887ec5cc775f7be1d5510b8fdd14856be6f
actor20170211030627 <1455198886@qq.com>
2020-04-08 15:45:05 +0800

    add BaseSlidingDrawer.java

--- app/src/main/java/com/actor/testapplication/activity/CustomViewActivity.java
+++ app/src/main/java/com/actor/testapplication/activity/CustomViewActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.actor.testapplication.R;
+import com.actor.testapplication.widget.BaseSlidingDrawer;
 import com.actor.testapplication.widget.ItemAddMinusLayout;
 
 import butterknife.BindView;
@@ -14,6 +15,8 @@ public class CustomViewActivity extends BaseActivity {
 
     @BindView(R.id.item_add_minus)
     ItemAddMinusLayout  itemAddMinus;
+    @BindView(R.id.sliding_drawer)
+    BaseSlidingDrawer slidingDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +25,17 @@ public class CustomViewActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         setTitle("自定义View");
-
     }
 
-    @OnClick({R.id.btn1})
+    @OnClick({R.id.btn1, R.id.view, R.id.ll_handle})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn1:
+            case R.id.ll_handle:
+                slidingDrawer.animateToggle();
+                break;
+            case R.id.view:
+                toast("view clicked!");
                 break;
         }
     }
