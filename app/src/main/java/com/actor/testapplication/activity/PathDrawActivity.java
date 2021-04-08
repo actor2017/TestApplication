d5431f4d6d9243ee4bfed722ecb6376c716d3817
actor20170211030627 <1455198886@qq.com>
2021-04-08 18:50:14 +0800

    path未完待续...

--- app/src/main/java/com/actor/testapplication/activity/PathDrawActivity.java
+++ app/src/main/java/com/actor/testapplication/activity/PathDrawActivity.java
@@ -1,12 +1,18 @@
 package com.actor.testapplication.activity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
 
+import com.actor.myandroidframework.widget.BaseSpinner;
 import com.actor.testapplication.R;
 import com.actor.testapplication.widget.PathView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Path绘制测试
@@ -20,6 +26,12 @@ import butterknife.ButterKnife;
  */
 public class PathDrawActivity extends BaseActivity {
 
+    @BindView(R.id.bs_paint_style)
+    BaseSpinner bsPaintStyle;
+    @BindView(R.id.et_x)
+    EditText etX;
+    @BindView(R.id.et_y)
+    EditText etY;
     @BindView(R.id.path_view)
     PathView pathView;
 
@@ -28,5 +40,41 @@ public class PathDrawActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_path_draw);
         ButterKnife.bind(this);
+        setTitle("Path绘制测试");
+        bsPaintStyle.setOnItemSelectedListener(new BaseSpinner.OnItemSelectedListener2() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 1:
+                        pathView.getPaint().setStyle(Paint.Style.FILL);//画实心圆?
+                        break;
+                    case 2:
+                        pathView.getPaint().setStyle(Paint.Style.STROKE);
+                        break;
+                    case 3:
+                        pathView.getPaint().setStyle(Paint.Style.FILL_AND_STROKE);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
+    @OnClick({R.id.btn_lineto, })
+    public void onViewClicked(View view) {
+        float etx = Float.parseFloat(getText(etX));
+        float ety = Float.parseFloat(getText(etY));
+        switch (view.getId()) {
+            case R.id.btn_lineto://lineTo
+                pathView.lineTo(etx, ety);
+                pathView.lineTo(100, 600);
+                pathView.lineTo(100, 800);
+                pathView.setLastPoint(500,300);
+                pathView.invalidate();
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/actor/testapplication/widget/PathView.java
+++ app/src/main/java/com/actor/testapplication/widget/PathView.java
@@ -5,11 +5,14 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.actor.testapplication.R;
 
@@ -22,11 +25,12 @@ import com.actor.testapplication.R;
  */
 public class PathView extends View {
 
-    protected Paint paint;//笔
+    protected Paint mPaint;//笔
 
     @ColorInt
-    protected int   color     = Color.CYAN;//颜色
-    protected float lineWidth = 2.0F;      //线宽
+    protected int   color     = Color.RED;  //颜色
+    protected float lineWidth = 5.0F;       //线宽
+    private   Path  mPath;
 
     public PathView(Context context) {
         super(context);
@@ -43,13 +47,16 @@ public class PathView extends View {
         init(context, attrs);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public PathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         init(context, attrs);
     }
 
     protected void init(Context context, @Nullable AttributeSet attrs) {
-        paint = new Paint(Paint.ANTI_ALIAS_FLAG);//抗锯齿
+        //创建Path对象
+        mPath = new Path();
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);//抗锯齿
         if (attrs != null) {
             //根据xml中属性, 给view赋值
             TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.PathView);
@@ -58,13 +65,23 @@ public class PathView extends View {
 
             typedArray.recycle();
         }
-        paint.setColor(color);
-        paint.setStrokeWidth(lineWidth);
+        mPaint.setColor(color);
+        mPaint.setStyle(Paint.Style.STROKE);//空心,填充模式 - 描边
+        mPaint.setStrokeWidth(lineWidth);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawPath(mPath, mPaint);//画出路径
+    }
+
+    public Paint getPaint() {
+        return mPaint;
+    }
+
+    public Path getPath() {
+        return mPath;
     }
 
     /**
@@ -80,4 +97,50 @@ public class PathView extends View {
     public void setLineWidth(float lineWidth) {
         this.lineWidth = lineWidth;
     }
+
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Path 直线与点的操作
+    ///////////////////////////////////////////////////////////////////////////
+    /**
+     * 移动到指定点(将下一次操作路径的起点移动到(x,y)), 可和lineTo混合调用
+     * 会影响下次操作，不会影响上一次操作
+     */
+    public void moveTo(float x, float y) {
+        mPath.moveTo(x, y);
+    }
+
+    /**
+     * 从当前点画一条直线到指定点
+     */
+    public void lineTo (float x, float y) {
+        mPath.lineTo(x, y);
+    }
+
+    /**
+     * 改变上一次操作路径的结束坐标点, 会影响下次操作，也会影响上一次操作
+     * path.lineTo(300, 300);       //创建一条从原点到坐标(300,300)的直线
+     * path.lineTo(100, 600);
+     * path.lineTo(100, 800);
+     * path.setLastPoint(500, 300);  //将上一次的操作路径的终点(100, 800)"移动"到(500, 300)
+     * 等价于: ==>
+     * path.lineTo(300, 300);
+     * path.lineTo(100, 600);
+     * path.lineTo(500, 300);
+     */
+    public void setLastPoint(float dx, float dy) {
+        mPath.setLastPoint(dx, dy);
+    }
+
+    /**
+     * 封闭当前路径
+     */
+    public void close() {
+        mPath.close();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Path 基本形状
+    ///////////////////////////////////////////////////////////////////////////
 }
